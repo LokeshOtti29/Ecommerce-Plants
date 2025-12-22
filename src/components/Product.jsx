1e72718c453d947feeb806c7212d70fa365c352b
LokeshOtti29 <lokeshotti456@gmail.com>
2025-12-22 16:55:31 +0530

    Ecommerce-ProductPage

---
+++ src/components/Product.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div className="product-card">
+      <div className="product-image">
+        <img src="" alt="product" />
+      </div>
+
+      <div className="product-details">
+        <div className="product-title"></div>
+        <div className="product-price"></div>
+        <div className="product-actions"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
